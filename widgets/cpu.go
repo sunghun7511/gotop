6f3986c389d3f4c8241edbc423066cb99e383f27
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-07-21 01:16:02 +0900

    refactor: reordering of methods

--- widgets/cpu.go
+++ widgets/cpu.go
@@ -30,6 +30,57 @@ type CpuWidget struct {
 
 var HORIZONTAL_SCALE = 3
 
+func NewCpuWidget() Widget {
+	plot := tWidgets.NewPlot()
+	plot.Title = " CPU Usage "
+	plot.AxesColor = tui.ColorWhite
+	plot.HorizontalScale = HORIZONTAL_SCALE
+	plot.ShowAxes = false
+	plot.MaxVal = 100
+
+	cpuStats := getCpuStats()
+
+	data := make([][]float64, cpuStats.cores)
+
+	termWidth, _ := tui.TerminalDimensions()
+	for i := 0; i < cpuStats.cores; i++ {
+		data[i] = make([]float64, termWidth/HORIZONTAL_SCALE+1)
+	}
+
+	plot.Data = data
+
+	return &CpuWidget{
+		cpuStats: cpuStats,
+		data:     data,
+		plot:     plot,
+	}
+}
+
+func (widget *CpuWidget) Update() {
+	currentCpuStats := getCpuStats()
+	previousCpuStats := widget.cpuStats
+
+	cores := currentCpuStats.cores
+	for core := 0; core < cores; core++ {
+		previousCoreStats := previousCpuStats.stats[core]
+		currentCoreStats := currentCpuStats.stats[core]
+
+		coreUsage := calculateCoreUsage(previousCoreStats, currentCoreStats)
+		widget.pushCoreUsageData(core, coreUsage)
+	}
+
+	widget.cpuStats = currentCpuStats
+}
+
+func (widget *CpuWidget) HandleSignal(event tui.Event) {
+	// is there anything to handle?
+}
+
+func (widget *CpuWidget) GetUI() tui.Drawable {
+	widget.plot.Data = widget.data
+	return widget.plot
+}
+
 // read and parse cpu usage data from /proc/stat
 func getCpuStats() CpuStats {
 	file, err := os.Open("/proc/stat")
@@ -84,58 +135,7 @@ func calculateCoreUsage(previousCoreStats, currentCoreStats CpuCoreStats) float6
 	return (float64(deltaUserProcessTime) / float64(deltaTotalTime)) * 100.0
 }
 
-func (widget *CpuWidget) pushData(core int, coreUsage float64) {
+func (widget *CpuWidget) pushCoreUsageData(core int, coreUsage float64) {
 	widget.data[core] = append(widget.data[core], coreUsage)
 	widget.data[core] = widget.data[core][1:]
-}
-
-func NewCpuWidget() Widget {
-	plot := tWidgets.NewPlot()
-	plot.Title = " CPU Usage "
-	plot.AxesColor = tui.ColorWhite
-	plot.HorizontalScale = HORIZONTAL_SCALE
-	plot.ShowAxes = false
-	plot.MaxVal = 100
-
-	cpuStats := getCpuStats()
-
-	data := make([][]float64, cpuStats.cores)
-
-	termWidth, _ := tui.TerminalDimensions()
-	for i := 0; i < cpuStats.cores; i++ {
-		data[i] = make([]float64, termWidth/HORIZONTAL_SCALE+1)
-	}
-
-	plot.Data = data
-
-	return &CpuWidget{
-		cpuStats: cpuStats,
-		data:     data,
-		plot:     plot,
-	}
-}
-
-func (widget *CpuWidget) Update() {
-	currentCpuStats := getCpuStats()
-	previousCpuStats := widget.cpuStats
-
-	cores := currentCpuStats.cores
-	for core := 0; core < cores; core++ {
-		previousCoreStats := previousCpuStats.stats[core]
-		currentCoreStats := currentCpuStats.stats[core]
-
-		coreUsage := calculateCoreUsage(previousCoreStats, currentCoreStats)
-		widget.pushData(core, coreUsage)
-	}
-
-	widget.cpuStats = currentCpuStats
-}
-
-func (widget *CpuWidget) HandleSignal(event tui.Event) {
-	// is there anything to handle?
-}
-
-func (widget *CpuWidget) GetUI() tui.Drawable {
-	widget.plot.Data = widget.data
-	return widget.plot
 }
