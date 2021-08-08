bfdca9cb2710c43465e67130f06299d8ec7c0d85
zych1751 <zych1751@naver.com>
2021-08-08 13:53:07 +0900

    fix: 올바르지 않은 표현 수정 (datas->data)

--- util/slice.go
+++ util/slice.go
@@ -1,8 +1,8 @@
 package util
 
-// PushUsageData datas 배열에 data를 추가하고 맨 앞 데이터를 삭제한다.
-func PushUsageData(datas []float64, data float64) []float64 {
-	datas = append(datas, data)
-	datas = datas[1:]
-	return datas
+// PushUsageData data 배열에 value를 추가하고 맨 앞 데이터를 삭제한다.
+func PushUsageData(data []float64, value float64) []float64 {
+	data = append(data, value)
+	data = data[1:]
+	return data
 }
